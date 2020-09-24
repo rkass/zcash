ZcashFoundation/zcash_script

Replace libsodium's crypto_generichash_blake2b with blake2b_simd

src/pow/tromp/equi.h
@@ -1,7 +1,6 @@
 // Equihash solver
 // Copyright (c) 2016-2016 John Tromp, The Zcash developers
 
-#include "sodium.h"
 #ifdef __APPLE__
 #include "pow/tromp/osx_barrier.h"
 #endif
@@ -10,6 +9,8 @@
 #include <stdint.h> // for types uint32_t,uint64_t
 #include <string.h> // for functions memset
 #include <stdlib.h> // for function qsort
+
+#include <rust/blake2b.h>
 
 typedef uint32_t u32;
 typedef unsigned char uchar;
@@ -39,16 +40,17 @@ typedef u32 proof[PROOFSIZE];
 enum verify_code { POW_OK, POW_DUPLICATE, POW_OUT_OF_ORDER, POW_NONZERO_XOR };
 const char *errstr[] = { "OK", "duplicate index", "indices out of order", "nonzero xor" };
 
-void genhash(const crypto_generichash_blake2b_state *ctx, u32 idx, uchar *hash) {
-  crypto_generichash_blake2b_state state = *ctx;
+void genhash(const BLAKE2bState *ctx, u32 idx, uchar *hash) {
+  auto state = rust_blake2b_clone(ctx);
   u32 leb = htole32(idx / HASHESPERBLAKE);
-  crypto_generichash_blake2b_update(&state, (uchar *)&leb, sizeof(u32));
+  rust_blake2b_update(state, (uchar *)&leb, sizeof(u32));
   uchar blakehash[HASHOUT];
-  crypto_generichash_blake2b_final(&state, blakehash, HASHOUT);
+  rust_blake2b_finalize(state, blakehash, HASHOUT);
+  rust_blake2b_free(state);
   memcpy(hash, blakehash + (idx % HASHESPERBLAKE) * WN/8, WN/8);
 }
 
-int verifyrec(const crypto_generichash_blake2b_state *ctx, u32 *indices, uchar *hash, int r) {
+int verifyrec(const BLAKE2bState *ctx, u32 *indices, uchar *hash, int r) {
   if (r == 0) {
     genhash(ctx, *indices, hash);
     return POW_OK;
@@ -90,7 +92,7 @@ bool duped(proof prf) {
 }
 
 // verify Wagner conditions
-int verify(u32 indices[PROOFSIZE], const crypto_generichash_blake2b_state *ctx) {
+int verify(u32 indices[PROOFSIZE], const BLAKE2bState *ctx) {
   if (duped(indices))
     return POW_DUPLICATE;
   uchar hash[WN/8];

src/zcash/JoinSplit.cpp
@@ -1,6 +1,5 @@
 #include "JoinSplit.hpp"
 #include "prf.h"
-#include "sodium.h"
 
 #include "zcash/util.h"
 
@@ -17,6 +16,8 @@
 #include "librustzcash.h"
 #include "streams.h"
 #include "version.h"
+
+#include <rust/blake2b.h>
 
 namespace libzcash {
 
@@ -218,7 +219,7 @@ uint256 JoinSplit<NumInputs, NumOutputs>::h_sig(
     const std::array<uint256, NumInputs>& nullifiers,
     const uint256& joinSplitPubKey
 ) {
-    const unsigned char personalization[crypto_generichash_blake2b_PERSONALBYTES]
+    const unsigned char personalization[BLAKE2bPersonalBytes]
         = {'Z','c','a','s','h','C','o','m','p','u','t','e','h','S','i','g'};
 
     std::vector<unsigned char> block(randomSeed.begin(), randomSeed.end());
@@ -231,15 +232,10 @@ uint256 JoinSplit<NumInputs, NumOutputs>::h_sig(
 
     uint256 output;
 
-    if (crypto_generichash_blake2b_salt_personal(output.begin(), 32,
-                                                 &block[0], block.size(),
-                                                 NULL, 0, // No key.
-                                                 NULL,    // No salt.
-                                                 personalization
-                                                ) != 0)
-    {
-        throw std::logic_error("hash function failure");
-    }
+    auto state = rust_blake2b_init(32, personalization);
+    rust_blake2b_update(state, &block[0], block.size());
+    rust_blake2b_finalize(state, output.begin(), 32);
+    rust_blake2b_free(state);
 
     return output;
 }

src/zcash/NoteEncryption.cpp
@@ -8,6 +8,8 @@
 #include "prf.h"
 #include "librustzcash.h"
 
+#include <rust/blake2b.h>
+
 #define NOTEENCRYPTION_CIPHER_KEYSIZE 32
 
 void clamp_curve25519(unsigned char key[crypto_scalarmult_SCALARBYTES])
@@ -31,18 +33,13 @@ void PRF_ock(
     memcpy(block+64, cm.begin(), 32);
     memcpy(block+96, epk.begin(), 32);
 
-    unsigned char personalization[crypto_generichash_blake2b_PERSONALBYTES] = {};
+    unsigned char personalization[BLAKE2bPersonalBytes] = {};
     memcpy(personalization, "Zcash_Derive_ock", 16);
 
-    if (crypto_generichash_blake2b_salt_personal(K, NOTEENCRYPTION_CIPHER_KEYSIZE,
-                                                 block, 128,
-                                                 NULL, 0, // No key.
-                                                 NULL,    // No salt.
-                                                 personalization
-                                                ) != 0)
-    {
-        throw std::logic_error("hash function failure");
-    }
+    auto state = rust_blake2b_init(NOTEENCRYPTION_CIPHER_KEYSIZE, personalization);
+    rust_blake2b_update(state, block, 128);
+    rust_blake2b_finalize(state, K, NOTEENCRYPTION_CIPHER_KEYSIZE);
+    rust_blake2b_free(state);
 }
 
 void KDF_Sapling(
@@ -55,18 +52,13 @@ void KDF_Sapling(
     memcpy(block+0, dhsecret.begin(), 32);
     memcpy(block+32, epk.begin(), 32);
 
-    unsigned char personalization[crypto_generichash_blake2b_PERSONALBYTES] = {};
+    unsigned char personalization[BLAKE2bPersonalBytes] = {};
     memcpy(personalization, "Zcash_SaplingKDF", 16);
 
-    if (crypto_generichash_blake2b_salt_personal(K, NOTEENCRYPTION_CIPHER_KEYSIZE,
-                                                 block, 64,
-                                                 NULL, 0, // No key.
-                                                 NULL,    // No salt.
-                                                 personalization
-                                                ) != 0)
-    {
-        throw std::logic_error("hash function failure");
-    }
+    auto state = rust_blake2b_init(NOTEENCRYPTION_CIPHER_KEYSIZE, personalization);
+    rust_blake2b_update(state, block, 64);
+    rust_blake2b_finalize(state, K, NOTEENCRYPTION_CIPHER_KEYSIZE);
+    rust_blake2b_free(state);
 }
 
 void KDF(unsigned char K[NOTEENCRYPTION_CIPHER_KEYSIZE],
@@ -87,19 +79,14 @@ void KDF(unsigned char K[NOTEENCRYPTION_CIPHER_KEYSIZE],
     memcpy(block+64, epk.begin(), 32);
     memcpy(block+96, pk_enc.begin(), 32);
 
-    unsigned char personalization[crypto_generichash_blake2b_PERSONALBYTES] = {};
+    unsigned char personalization[BLAKE2bPersonalBytes] = {};
     memcpy(personalization, "ZcashKDF", 8);
     memcpy(personalization+8, &nonce, 1);
 
-    if (crypto_generichash_blake2b_salt_personal(K, NOTEENCRYPTION_CIPHER_KEYSIZE,
-                                                 block, 128,
-                                                 NULL, 0, // No key.
-                                                 NULL,    // No salt.
-                                                 personalization
-                                                ) != 0)
-    {
-        throw std::logic_error("hash function failure");
-    }
+    auto state = rust_blake2b_init(NOTEENCRYPTION_CIPHER_KEYSIZE, personalization);
+    rust_blake2b_update(state, block, 128);
+    rust_blake2b_finalize(state, K, NOTEENCRYPTION_CIPHER_KEYSIZE);
+    rust_blake2b_free(state);
 }
 
 namespace libzcash {

rkass/zcash

src/pow/tromp/equi.h
@@ -1,7 +1,6 @@
 // Equihash solver
 // Copyright (c) 2016-2016 John Tromp, The Zcash developers
 
-#include "sodium.h"
 #ifdef __APPLE__
 #include "pow/tromp/osx_barrier.h"
 #endif
@@ -10,6 +9,8 @@
 #include <stdint.h> // for types uint32_t,uint64_t
 #include <string.h> // for functions memset
 #include <stdlib.h> // for function qsort
+
+#include <rust/blake2b.h>
 
 typedef uint32_t u32;
 typedef unsigned char uchar;
@@ -39,16 +40,17 @@ typedef u32 proof[PROOFSIZE];
 enum verify_code { POW_OK, POW_DUPLICATE, POW_OUT_OF_ORDER, POW_NONZERO_XOR };
 const char *errstr[] = { "OK", "duplicate index", "indices out of order", "nonzero xor" };
 
-void genhash(const crypto_generichash_blake2b_state *ctx, u32 idx, uchar *hash) {
-  crypto_generichash_blake2b_state state = *ctx;
+void genhash(const BLAKE2bState *ctx, u32 idx, uchar *hash) {
+  auto state = blake2b_clone(ctx);
   u32 leb = htole32(idx / HASHESPERBLAKE);
-  crypto_generichash_blake2b_update(&state, (uchar *)&leb, sizeof(u32));
+  blake2b_update(state, (uchar *)&leb, sizeof(u32));
   uchar blakehash[HASHOUT];
-  crypto_generichash_blake2b_final(&state, blakehash, HASHOUT);
+  blake2b_finalize(state, blakehash, HASHOUT);
+  blake2b_free(state);
   memcpy(hash, blakehash + (idx % HASHESPERBLAKE) * WN/8, WN/8);
 }
 
-int verifyrec(const crypto_generichash_blake2b_state *ctx, u32 *indices, uchar *hash, int r) {
+int verifyrec(const BLAKE2bState *ctx, u32 *indices, uchar *hash, int r) {
   if (r == 0) {
     genhash(ctx, *indices, hash);
     return POW_OK;
@@ -90,7 +92,7 @@ bool duped(proof prf) {
 }
 
 // verify Wagner conditions
-int verify(u32 indices[PROOFSIZE], const crypto_generichash_blake2b_state *ctx) {
+int verify(u32 indices[PROOFSIZE], const BLAKE2bState *ctx) {
   if (duped(indices))
     return POW_DUPLICATE;
   uchar hash[WN/8];

src/zcash/JoinSplit.cpp
@@ -1,6 +1,5 @@
 #include "JoinSplit.hpp"
 #include "prf.h"
-#include "sodium.h"
 
 #include "zcash/util.h"
 
@@ -17,6 +16,8 @@
 #include "librustzcash.h"
 #include "streams.h"
 #include "version.h"
+
+#include <rust/blake2b.h>
 
 namespace libzcash {
 
@@ -207,7 +208,7 @@ uint256 JoinSplit<NumInputs, NumOutputs>::h_sig(
     const std::array<uint256, NumInputs>& nullifiers,
     const Ed25519VerificationKey& joinSplitPubKey
 ) {
-    const unsigned char personalization[crypto_generichash_blake2b_PERSONALBYTES]
+    const unsigned char personalization[BLAKE2bPersonalBytes]
         = {'Z','c','a','s','h','C','o','m','p','u','t','e','h','S','i','g'};
 
     std::vector<unsigned char> block(randomSeed.begin(), randomSeed.end());
@@ -220,15 +221,10 @@ uint256 JoinSplit<NumInputs, NumOutputs>::h_sig(
 
     uint256 output;
 
-    if (crypto_generichash_blake2b_salt_personal(output.begin(), 32,
-                                                 &block[0], block.size(),
-                                                 NULL, 0, // No key.
-                                                 NULL,    // No salt.
-                                                 personalization
-                                                ) != 0)
-    {
-        throw std::logic_error("hash function failure");
-    }
+    auto state = blake2b_init(32, personalization);
+    blake2b_update(state, &block[0], block.size());
+    blake2b_finalize(state, output.begin(), 32);
+    blake2b_free(state);
 
     return output;
 }

src/zcash/NoteEncryption.cpp
@@ -8,6 +8,8 @@
 #include "prf.h"
 #include "librustzcash.h"
 
+#include <rust/blake2b.h>
+
 #define NOTEENCRYPTION_CIPHER_KEYSIZE 32
 
 void clamp_curve25519(unsigned char key[crypto_scalarmult_SCALARBYTES])
@@ -31,18 +33,13 @@ void PRF_ock(
     memcpy(block+64, cm.begin(), 32);
     memcpy(block+96, epk.begin(), 32);
 
-    unsigned char personalization[crypto_generichash_blake2b_PERSONALBYTES] = {};
+    unsigned char personalization[BLAKE2bPersonalBytes] = {};
     memcpy(personalization, "Zcash_Derive_ock", 16);
 
-    if (crypto_generichash_blake2b_salt_personal(K, NOTEENCRYPTION_CIPHER_KEYSIZE,
-                                                 block, 128,
-                                                 NULL, 0, // No key.
-                                                 NULL,    // No salt.
-                                                 personalization
-                                                ) != 0)
-    {
-        throw std::logic_error("hash function failure");
-    }
+    auto state = blake2b_init(NOTEENCRYPTION_CIPHER_KEYSIZE, personalization);
+    blake2b_update(state, block, 128);
+    blake2b_finalize(state, K, NOTEENCRYPTION_CIPHER_KEYSIZE);
+    blake2b_free(state);
 }
 
 void KDF_Sapling(
@@ -55,18 +52,13 @@ void KDF_Sapling(
     memcpy(block+0, dhsecret.begin(), 32);
     memcpy(block+32, epk.begin(), 32);
 
-    unsigned char personalization[crypto_generichash_blake2b_PERSONALBYTES] = {};
+    unsigned char personalization[BLAKE2bPersonalBytes] = {};
     memcpy(personalization, "Zcash_SaplingKDF", 16);
 
-    if (crypto_generichash_blake2b_salt_personal(K, NOTEENCRYPTION_CIPHER_KEYSIZE,
-                                                 block, 64,
-                                                 NULL, 0, // No key.
-                                                 NULL,    // No salt.
-                                                 personalization
-                                                ) != 0)
-    {
-        throw std::logic_error("hash function failure");
-    }
+    auto state = blake2b_init(NOTEENCRYPTION_CIPHER_KEYSIZE, personalization);
+    blake2b_update(state, block, 64);
+    blake2b_finalize(state, K, NOTEENCRYPTION_CIPHER_KEYSIZE);
+    blake2b_free(state);
 }
 
 void KDF(unsigned char K[NOTEENCRYPTION_CIPHER_KEYSIZE],
@@ -87,19 +79,14 @@ void KDF(unsigned char K[NOTEENCRYPTION_CIPHER_KEYSIZE],
     memcpy(block+64, epk.begin(), 32);
     memcpy(block+96, pk_enc.begin(), 32);
 
-    unsigned char personalization[crypto_generichash_blake2b_PERSONALBYTES] = {};
+    unsigned char personalization[BLAKE2bPersonalBytes] = {};
     memcpy(personalization, "ZcashKDF", 8);
     memcpy(personalization+8, &nonce, 1);
 
-    if (crypto_generichash_blake2b_salt_personal(K, NOTEENCRYPTION_CIPHER_KEYSIZE,
-                                                 block, 128,
-                                                 NULL, 0, // No key.
-                                                 NULL,    // No salt.
-                                                 personalization
-                                                ) != 0)
-    {
-        throw std::logic_error("hash function failure");
-    }
+    auto state = blake2b_init(NOTEENCRYPTION_CIPHER_KEYSIZE, personalization);
+    blake2b_update(state, block, 128);
+    blake2b_finalize(state, K, NOTEENCRYPTION_CIPHER_KEYSIZE);
+    blake2b_free(state);
 }
 
 namespace libzcash {