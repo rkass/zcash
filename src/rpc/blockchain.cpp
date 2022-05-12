e2e7d0f86a0026e3d37b868fb9d37d7e0d6fef0a
Ryan Kass <kassr30@gmail.com>
2022-05-12 15:19:37 -0600

    experiment

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -748,8 +748,8 @@ UniValue getserializedblock(const UniValue& params, bool fHelp)
     UniValue item(UniValue::VOBJ);
     item.pushKV("block", univalueFromBlk(block));
     item.pushKV("timestamp", block.GetBlockTime());
-    item.pushKV("hash", (int64_t) block.GetHash());
-    item.pushKV("parent", (int64_t) block.hashPrevBlock));
+    item.pushKV("hash", (int64_t) ((uint64_t) block.GetHash()));
+    item.pushKV("parent", (int64_t) ((unit64_t) block.hashPrevBlock));
     result.push_back(item);
 
     return result;
